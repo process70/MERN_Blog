d6d5f92c754fe2bd8e74b43fd3e99116a575e21f
process70 <163150253+process70@users.noreply.github.com>
2024-11-15 19:56:48 +0000

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -45,4 +45,9 @@ app.use(errorHandler)
 //         message: "Not Found"+req.originalUrl
 //     })
 // })
+// Serve static files from the React/Vite app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../frontend/build/index.html'))});
 
