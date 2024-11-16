4466453d6409673163445111a98fd5ab5b23ff06
process70 <163150253+process70@users.noreply.github.com>
2024-11-16 13:50:58 +0000

    Add files via upload

---
+++ frontend/src/Components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className="loader">
+        <div className="loader__image">
+            <img src="https://i.ibb.co/ykcTvBZ/loading.gif" alt="loading thumbnail" />
+        </div>
+    </div>
+  )
+}
+
+export default Loader
---
+++ frontend/src/Components/PostItem.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PostAuthor from './PostAuthor'
+
+function PostItem({postId, thumbnail, category, title, description, creator, created}) {
+
+  return (
+    <article className="post">
+        <div className="post__thumbnail">
+            <img src={`${thumbnail}`} alt={title} />
+        </div>
+        <div className="post_content">
+            <Link to={`/posts/${postId}`}>
+                <h3>{title}</h3>
+            </Link>
+            <p dangerouslySetInnerHTML={{__html: description.substr(0, 100)}} />
+            <div className="post__footer">
+                <PostAuthor creator={creator} created={created}/>
+                <Link to={`/posts/categories/${category}`} className='btn category'>{category}</Link>
+            </div>
+        </div>
+    </article>
+  )
+}
+
+export default PostItem
